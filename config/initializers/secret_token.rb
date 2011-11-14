2e587b0f4c8a6302a2920f49de89946ebac0dde5
Thomas Cullen <cullentj@yahoo.com>
2011-11-14 17:42:47 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TomsAhaList::Application.config.secret_token = 'ec216e2308375793e090213089af589a1c346b6ec56ff4c371a189b9dba13af2b5a35d1ce1dfa618891cfffb1601235d7238ece2c4696f4b160ed08d10e4b34a'
